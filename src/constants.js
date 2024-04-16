6f93e50401a0013043fe987030e3201f6b237538
Eric Chun <ericjiminy@gmail.com>
2024-04-16 13:11:00 -0400

    Revert "Updates"
    This reverts commit aedcf386454fc3d2863f7d0566daf4a236233c2e.

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const assets_data = [
+  ["Checkings", 0],
+  ["Savings", 0],
+  ["401k", 0],
+];
+
+export const liabilities_data = [
+  ["Mortgage", 0],
+  ["Car Loan", 0],
+  ["Student Loan", 0],
+];
+
+export const input_error_types = ["none", "empty", "number", "commas"];
+
+export const input_error_texts = [
+  "Value can not be empty",
+  "Value must be a valid number",
+  "Value can not include commas",
+];
